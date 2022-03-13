b1d3e5d8140c28e843b8d7a6c45ce3be5067080d
Franco Gonzalez Frau <frangf2xxx@gmail.com>
2022-03-13 18:19:38 -0300

    cargando añadido

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -2,15 +2,18 @@ import React from "react";
 
 const ItemDetail = (props) => {
   return (
-    <div className="itemDetail">
-      <div className="itemDetail__name">{props.item.nombre}</div>
-      <img src={props.item.imagen} alt="" />
-      <div className="itemDetail__editorial">{props.item.editorial}</div>
-      <div className="itemDetail__decription">{props.item.descripción}</div>
-      <div className="itemDetail__pages">{props.item.páginas}</div>
-      <div className="itemDetail__price">${props.item.precio}</div>
-      <div className="itemDetail__stock">{props.item.stock}u</div>
-    </div>
+    <>
+      <p>{props.carga ? "Cargando." : "Detalles del producto"}</p>
+      <div className="itemDetail">
+        <div className="itemDetail__name">{props.item.nombre}</div>
+        <img src={props.item.imagen} alt="" />
+        <div className="itemDetail__editorial">{props.item.editorial}</div>
+        <div className="itemDetail__decription">{props.item.descripción}</div>
+        <div className="itemDetail__pages">{props.item.páginas}</div>
+        <div className="itemDetail__price">{props.item.precio}</div>
+        <div className="itemDetail__stock">{props.item.stock}</div>
+      </div>
+    </>
   );
 };
 
