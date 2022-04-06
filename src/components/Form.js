a8d4c66eb0c04e0ffc87e332ffe5a947429f9ad1
Franco Gonzalez Frau <frangf2xxx@gmail.com>
2022-04-06 17:40:50 -0300

    form implementado

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,12 @@
 import React, { useContext, useState } from "react";
 import { context } from "./CartContext";
+import { serverTimestamp, collection, addDoc } from "firebase/firestore";
+import { db } from "./Firebase";
 
 const Form = () => {
-  const resultado = useContext(context);
-  const terminarCompra = resultado.terminarCompra;
-  const clienteAux = {
-    nombre: "",
-    apellido: "",
-    email: "",
-    telefono: "",
-  };
+  const { carrito, total } = useContext(context);
+  const [carga, setCarga] = useState(false);
+  const [idPedido, setIdPedido] = useState("");
   const [cliente, setCliente] = useState({
     nombre: "",
     apellido: "",
@@ -17,60 +14,101 @@ const Form = () => {
     telefono: "",
   });
 
+  const terminarCompra = async (e) => {
+    e.preventDefault();
+    console.log("cliente", cliente);
+    console.log("cart", carrito);
+    const orden = {
+      buyer: {
+        name: cliente.nombre,
+        lastName: cliente.apellido,
+        email: cliente.email,
+        phone: cliente.telefono,
+      },
+      items: carrito,
+      total: total,
+      date: serverTimestamp(),
+    };
+    console.log("orden", orden);
+
+    if (
+      carrito.length > 0 &&
+      cliente.nombre.length > 0 &&
+      cliente.apellido.length > 0 &&
+      cliente.email.length > 0 &&
+      cliente.telefono.length > 0
+    ) {
+      const pedido = await addDoc(collection(db, "Ordenes"), orden);
+      setCarga(true);
+      setIdPedido(pedido.id);
+    } else {
+      alert("Ocurrio un error, revise sus datos ingresados");
+    }
+  };
+
   return (
-    <div>
-      <h1>Formulario de compra</h1>
-      <form>
-        <label>
-          Nombre:
-          <input
-            type="text"
-            name="nombre"
-            onChange={(e) => {
-              clienteAux.nombre = e.target.value;
-            }}
-          />
-        </label>
-        <label>
-          Apellido:
-          <input
-            type="text"
-            name="apellido"
-            onChange={(e) => {
-              clienteAux.apellido = e.target.value;
-            }}
-          />
-        </label>
-        <label>
-          Email:
-          <input
-            type="email"
-            name="email"
-            onChange={(e) => {
-              clienteAux.email = e.target.value;
-            }}
-          />
-        </label>
-        <label>
-          Teléfono:
-          <input
-            type="tel"
-            name="telefono"
-            onChange={(e) => {
-              clienteAux.telefono = e.target.value;
-            }}
-          />
-        </label>
-        <button
-          onClick={() => {
-            setCliente(clienteAux);
-            terminarCompra(cliente);
-          }}
-        >
-          Finalizar
-        </button>
-      </form>
-    </div>
+    <>
+      {carga ? (
+        <div>
+          <h2>Gracias por su compra</h2>
+          <p>
+            Su numero de orden es {idPedido}, en breve nos comunicaremos con
+            usted para confirmar su pedido
+          </p>
+        </div>
+      ) : (
+        <div>
+          <h1>Formulario de compra</h1>
+          <form>
+            <label>
+              Nombre:
+              <input
+                required
+                type="text"
+                name="nombre"
+                onChange={(e) =>
+                  setCliente({ ...cliente, nombre: e.target.value })
+                }
+              />
+            </label>
+            <label>
+              Apellido:
+              <input
+                required
+                type="text"
+                name="apellido"
+                onChange={(e) =>
+                  setCliente({ ...cliente, apellido: e.target.value })
+                }
+              />
+            </label>
+            <label>
+              Email:
+              <input
+                required
+                type="email"
+                name="email"
+                onChange={(e) =>
+                  setCliente({ ...cliente, email: e.target.value })
+                }
+              />
+            </label>
+            <label>
+              Teléfono:
+              <input
+                required
+                type="tel"
+                name="telefono"
+                onChange={(e) =>
+                  setCliente({ ...cliente, telefono: e.target.value })
+                }
+              />
+            </label>
+            <button onClick={(e) => terminarCompra(e)}>Finalizar</button>
+          </form>
+        </div>
+      )}
+    </>
   );
 };
 
